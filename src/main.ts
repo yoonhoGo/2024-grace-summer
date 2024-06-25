2c952571a66670997c09a6f886d6162cd935704d
WooSangWook <raincoat@kakao.com>
2024-06-25 16:35:05 +0900

    project init

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import PrimeVue from "primevue/config";
+import Aura from "@primevue/themes/aura";
+import Button from "primevue/button";
+
+const app = createApp(App);
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+});
+app.component("Button", Button);
+
+app.mount("#app");
