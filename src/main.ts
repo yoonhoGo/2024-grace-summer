86f1223841198bb8e588902c853030d11db77526
WooSangWook <raincoat@kakao.com>
2024-06-25 17:12:55 +0900

    Feat: add pinia store

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,12 @@ import PrimeVue from "primevue/config";
 import Aura from "@primevue/themes/aura";
 import Button from "primevue/button";
 import router from "./router";
+import { createPinia } from "pinia";
 
+const pinia = createPinia();
 const app = createApp(App);
 app.use(router);
+app.use(pinia);
 
 app.use(PrimeVue, {
   theme: {
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,23 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { useCounterStore } from "@/store/counter";
+import { storeToRefs } from "pinia";
+
+const store = useCounterStore();
+
+const { count, name, doubleCount } = storeToRefs(store);
+// increment 액션은 그냥 구조화 가능.
+const { increment } = store;
+</script>
 
 <template>
   <div>
     <h1>Home</h1>
     <p>This is the home page</p>
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">Go to About</router-link>
+    <p>Count: {{ count }}</p>
+    <p>Name: {{ name }}</p>
+    <p>Double count: {{ doubleCount }}</p>
+    <button @click="increment">Increment</button>
   </div>
 </template>
 
