3f90206f9e4cf633bbecf6bb9a223fe5da18e737
WooSangWook <raincoat@kakao.com>
2024-06-26 15:09:38 +0900

    Fix: build settings

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,11 @@ export default defineConfig({
       "@": resolve(__dirname, "src"),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[name].[hash][extname]",
+      },
+    },
+  },
 });
